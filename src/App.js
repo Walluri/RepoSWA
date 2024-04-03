93437e0b4f553d34e36a92696def8a7bbbb6145c
vallurisatyaASA <Veeravenkata.Valluri@AirservicesAustralia.com>
2024-04-03 17:36:07 +1100

    added msal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import {
+  AuthenticatedTemplate,
+  useMsal,
+  useMsalAuthentication,
+} from "@azure/msal-react";
+import { InteractionType } from "@azure/msal-browser";
 
 function App() {
-  const value = "World - 4";
-  return <div>Hello {value}</div>;
+  const { accounts } = useMsal();
+  useMsalAuthentication(InteractionType.Redirect);
+
+  return (
+    <AuthenticatedTemplate>
+      Hello {`Hello, ${accounts[0]?.idTokenClaims?.name}`}
+    </AuthenticatedTemplate>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import { LogLevel } from "@azure/msal-browser";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export const msalConfig = {
+  auth: {
+    clientId: "a516940e-a4d4-496c-af30-51c702d8816a",
+    authority:
+      "https://login.microsoftonline.com/050f7cda-dc40-4484-8262-54d596ffa869",
+    redirectUri: "https://yellow-glacier-07680ff00.5.azurestaticapps.net/",
+  },
+  cache: {
+    cacheLocation: "sessionStorage", // This configures where your cache will be stored
+    storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback: (level, message, containsPii) => {
+        if (containsPii) {
+          return;
+        }
+        switch (level) {
+          case LogLevel.Error:
+            console.error(message);
+            return;
+          case LogLevel.Info:
+            console.info(message);
+            return;
+          case LogLevel.Verbose:
+            console.debug(message);
+            return;
+          case LogLevel.Warning:
+            console.warn(message);
+            return;
+          default:
+            return;
+        }
+      },
+    },
+  },
+};
+
+const msalInstance = new PublicClientApplication(msalConfig);
+
+ReactDOM.render(
+  <MsalProvider instance={msalInstance}>
+    <App />
+  </MsalProvider>,
+  document.getElementById("root")
+);
